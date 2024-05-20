5245f9893548b11b7db976cec883ff4d9bd0dec8
Brett <127159734+sourdoughbredd@users.noreply.github.com>
2024-05-20 14:50:09 -0400

    Create basic header

---
+++ src/components/Header.jsx
@@ -0,0 +1,28 @@
+import styles from "./Header.module.css";
+
+const Header = () => {
+  return (
+    <>
+      <header className={styles.header}>
+        <a href="/">
+          <h1 className={styles.logo}>Brett Bussell</h1>
+        </a>
+        <nav className={styles.nav}>
+          <ul>
+            <li>
+              <a href="/about">About</a>
+            </li>
+            <li>
+              <a href="/contact">Contact</a>
+            </li>
+            <li>
+              <a href="/projects">Projects</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+};
+
+export default Header;
