465110314f78cdd448fc079b97c79aab8baac5a0
Brett <127159734+sourdoughbredd@users.noreply.github.com>
2024-05-28 11:37:19 -0400

    Generalize project showcase componenet for use on more pages

---
+++ src/components/ProjectShowcase.jsx
@@ -0,0 +1,17 @@
+import styles from "./ProjectShowcase.module.css";
+import ProjectCard from "./ProjectCard.jsx";
+
+const ProjectShowcase = ({ sectionTitle, projects }) => {
+  return (
+    <div className={styles["section-container"]}>
+      <h2>{sectionTitle}</h2>
+      <div className={styles["projects-container"]}>
+        {projects.map((project) => (
+          <ProjectCard key={project.title} {...project} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ProjectShowcase;
