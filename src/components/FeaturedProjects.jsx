c65bce9ac7d1fe88f23148bc51be99ef49e1159d
Brett <127159734+sourdoughbredd@users.noreply.github.com>
2024-05-28 10:21:09 -0400

    Move project card component to own file

--- src/components/FeaturedProjects.jsx
+++ src/components/FeaturedProjects.jsx
@@ -1,66 +1,20 @@
 import styles from "./FeaturedProjects.module.css";
-import projects from "../data/projects.js";
-
-const Project = ({
-  title,
-  description,
-  features,
-  img,
-  tech,
-  repo_link,
-  live_link,
-  private_repo,
-}) => {
-  return (
-    <div className={styles.project}>
-      <div className={styles["project-content-container"]}>
-        <img src={img} alt="" className={styles.screenshot} />
-        <div className={styles["project-info-container"]}>
-          <h3>{title}</h3>
-          <p>{description}</p>
-          <h4>Features</h4>
-          <ul>
-            {features.map((feature) => (
-              <li key={feature}>{feature}</li>
-            ))}
-          </ul>
-          <div className={styles["tech-stack-container"]}>
-            <h4>Tech Stack</h4>
-            <span>
-              {tech.reduce((acc, curr, idx) => {
-                return acc + " " + curr + (idx < tech.length - 1 ? "," : "");
-              }, "")}
-            </span>
-          </div>
-
-          {private_repo ? (
-            <div className={styles.buttons}>
-              <a>
-                This project is part of an ongoing course at UC Berkeley. As
-                such, the code must be kept private. Contact me to request
-                access.
-              </a>
-            </div>
-          ) : (
-            <div className={styles.buttons}>
-              <a href={live_link}>Live Preview</a>
-              <a href={repo_link}>Github Repo</a>
-            </div>
-          )}
-        </div>
-      </div>
-    </div>
-  );
-};
+import ProjectCard from "./ProjectCard.jsx";
+import { featuredProjects } from "../data/projects.js";
 
 const FeaturedProjects = () => {
   return (
     <div className={styles["section-container"]}>
       <h2>Featured Projects</h2>
       <div className={styles["projects-container"]}>
-        {projects.map((project) => (
-          <Project key={project.title} {...project} />
+        {featuredProjects.map((project) => (
+          <ProjectCard key={project.title} {...project} />
         ))}
+      </div>
+      <div className={styles["see-more"]}>
+        <span>
+          <a href="/projects">Click here to see more projects</a>
+        </span>
       </div>
     </div>
   );
---
+++ src/components/ProjectCard.jsx
@@ -0,0 +1,55 @@
+import styles from "./ProjectCard.module.css";
+
+const Project = ({
+  title,
+  description,
+  features,
+  img,
+  tech,
+  repo_link,
+  live_link,
+  private_repo,
+}) => {
+  return (
+    <div className={styles.project}>
+      <div className={styles["project-content-container"]}>
+        <img src={img} alt="" className={styles.screenshot} />
+        <div className={styles["project-info-container"]}>
+          <h3>{title}</h3>
+          <p>{description}</p>
+          <h4>Features</h4>
+          <ul>
+            {features.map((feature) => (
+              <li key={feature}>{feature}</li>
+            ))}
+          </ul>
+          <div className={styles["tech-stack-container"]}>
+            <h4>Tech Stack</h4>
+            <span>
+              {tech.reduce((acc, curr, idx) => {
+                return acc + " " + curr + (idx < tech.length - 1 ? "," : "");
+              }, "")}
+            </span>
+          </div>
+
+          {private_repo ? (
+            <div className={styles.buttons}>
+              <a>
+                This project is part of an ongoing course at UC Berkeley. As
+                such, the code must be kept private. Contact me to request
+                access.
+              </a>
+            </div>
+          ) : (
+            <div className={styles.buttons}>
+              <a href={live_link}>Live Preview</a>
+              <a href={repo_link}>Github Repo</a>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Project;
