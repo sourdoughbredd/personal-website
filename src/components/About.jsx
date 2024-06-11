c8eaab80e22831e1d2c626aefd4876ff8e8ab0b1
Brett <127159734+sourdoughbredd@users.noreply.github.com>
2024-06-11 19:38:43 -0400

    Improve about me content

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,40 +3,38 @@ import styles from "./About.module.css";
 const About = () => {
   return (
     <div className={styles["about-me-container"]}>
-      <h2>About Me</h2>
+      <h2 className="section-title">About Me</h2>
       <div className={styles.paragraphs}>
         <div>
           <p>
-            Over the past 12 years, I've gained a wealth of experience across
-            various fields, including working on an almond ranch, a short career
-            as a mortgage loan officer, earning an M.S. in Mechanical
-            Engineering from UC Berkeley, and serving as a flight controls
-            engineer for the U.S. Navy. Through these diverse experiences, I
-            developed a profound passion for coding, which led me to fully
-            commit to a career in software development.
+            Over the past 12 years, I’ve done a bit of everything—from working
+            on an almond ranch to working as a mortgage loan officer, earning an
+            M.S. in Mechanical Engineering from UC Berkeley, and even working on
+            fighter jets as a flight controls engineer for the U.S. Navy.
+            Through all of these experiences, I found my true passion: coding.
+            This passion led me to dive headfirst into a career in software
+            development.
           </p>
           <p>
-            My journey has equipped me with the resilience and adaptability
-            needed to tackle complex projects and deliver high-quality
-            solutions. I bring a diverse set of skills and experiences to every
-            project, allowing me to approach challenges with a fresh and
-            creative mindset.
+            My background is pretty diverse, which has helped me develop the
+            discipline and problem-solving skills needed to handle complex
+            projects. Having done so many different things, I tend to see
+            challenges from a unique angle, which helps me come up with creative
+            and effective solutions.
           </p>
         </div>
         <div>
           <p>
-            As a full-stack developer, I am passionate about creating robust,
-            user-friendly applications with well-organized code. I excel in
-            technical presentation and communication, ensuring that complex
-            ideas are effectively conveyed to the targeted audience. Whether
-            it's developing web applications, optimizing performance, or
-            implementing new features, I am committed to excellence in every
-            aspect of my work.
+            As a full-stack developer, I love building robust, user-friendly
+            applications. I enjoy writing clean, well-organized code, and I’m
+            good at explaining technical concepts in a way that’s easy to
+            understand. Whether I’m developing web applications or perfecting an
+            espresso recipe at home, I aim for excellence in everything I do.
           </p>
           <p>
-            I'm always excited to take on new challenges and collaborate with
-            others who share my passion for technology. Let's connect and make
-            something amazing together!
+            I’m always up for new challenges and enjoy working with others who
+            are passionate about technology. Let’s connect and create something
+            awesome together!
           </p>
         </div>
       </div>
